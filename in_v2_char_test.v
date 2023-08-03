ae97355387f20a3a72eaa33a4318642f922417ed
bhendi-boi <bhendiisgreen@gmail.com>
2023-08-03 07:01:45 +0530

    works with 2 neurons +ve and -ve

--- in_v2_char_test.v
+++ in_v2_char_test.v
@@ -1,12 +1,11 @@
 `timescale 1ns / 1ps
-
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
 // 
-// Create Date: 16.06.2023 14:16:38
+// Create Date: 27.06.2023 21:37:06
 // Design Name: 
-// Module Name: in_v2_char_test
+// Module Name: ecg_encoder
 // Project Name: 
 // Target Devices: 
 // Tool Versions: 
@@ -21,33 +20,42 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module in_v2_char_test();
+module ecg_encoder();
 
-    reg CLK,KEY;
-    wire [19:0] v1new, u1new;
-    wire spike;
-    wire wea = 0;
-    wire [19:0] out;
-    reg [11:0] addra;
+    reg signed [19:0]I_ecg, I_ecg_neg ;
+    wire signed [19:0]Vn1,Un1, Vn2, Un2;
+    wire spike,spike1 ,spike2;
+    reg clk, reset;
+    reg [11:0]  k;
 
-    blk_mem_gen_0 a1(.clka(CLK),.wea(wea),.addra(addra),.douta(out));
+    reg [19:0] ecg[3596:0];
 
-    IN_V2_Char N1(CLK,KEY,out,v1new,u1new,spike);
-
-    initial begin 
-        KEY = 0; CLK = 0;addra=0;
-        #10 KEY = 1;
-        #10 KEY = 0; 
+    initial begin
+        $readmemh("C:/Users/KRISHNA/Desktop/study material/ECG/samples/mit107.txt", ecg);
     end
 
-    always#(1)begin
-        CLK = ~CLK;
+    IN_V2_Char uut1(clk,reset,I_ecg,Vn1,Un1,spike1);
+    IN_V2_Char uut2(clk,reset,I_ecg_neg,Vn2,Un2,spike2);
+
+    assign spike = spike1+spike2;
+
+    initial begin
+        clk=0;reset =0 ;I_ecg =20'sh0_0000 ;I_ecg_neg =20'sh0_FFFF;k=0;
+        #10 reset = 1;
+        #10 reset = 0;    
     end
-    always@(posedge CLK)begin
-        assign addra = addra + 12'b1;
+
+    always #(1) begin                          
+        clk = ~clk;
+        k = k + 12'b1;
+        I_ecg = ecg[k];
+        I_ecg_neg = (20'shF_FFFF - I_ecg);
+        if (k == 12'he06) begin
+            $finish();
+        end 
     end
+
 
 
 endmodule
-
 
