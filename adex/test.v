4afd02066c8976493e709c94d4c20ce4a528d5dd
bhendi-boi <bhendiisgreen@gmail.com>
2023-09-01 10:47:45 +0530

    formatted pradeep's code

--- adex/test.v
+++ adex/test.v
@@ -22,23 +22,23 @@
 
 module test();
 
-reg clk,rst;
-wire V_B,V;
-reg [22:0]I_B,I;
+    reg clk,rst;
+    wire V_B,V;
+    reg [22:0]I_B,I;
 
-// 7,16,3,3,9,10,23'sh0_0780_3,23'shF_EDED_3,38'sh0_0000_0002_B normal
-AdEx #(7,16,20,13,9,14,23'sh0_7828_4,23'shF_EE14_8,38'sh0_0000_0003_A)N(clk,rst,I,V);
-//AdEx #(7,16,9,10,9,10,23'sh0_0780_3,23'shF_F439_5,38'sh0_0009_0002_B)B(clk,rst,I_B,V_B);
+    // 7,16,3,3,9,10,23'sh0_0780_3,23'shF_EDED_3,38'sh0_0000_0002_B normal
+    AdEx #(7,16,20,13,9,14,23'sh0_7828_4,23'shF_EE14_8,38'sh0_0000_0003_A)N(clk,rst,I,V);
+    //AdEx #(7,16,9,10,9,10,23'sh0_0780_3,23'shF_F439_5,38'sh0_0009_0002_B)B(clk,rst,I_B,V_B);
 
 
-initial begin 
-clk = 0;rst = 1;I_B=0;I=0;
-#10 rst = 0;
-#10 rst = 1;
-//#20 I_B = 23'sh0_0000_8; I = 23'sh0_000A_0;
-end
+    initial begin 
+        clk = 0;rst = 1;I_B=0;I=0;
+        #10 rst = 0;
+        #10 rst = 1;
+        //#20 I_B = 23'sh0_0000_8; I = 23'sh0_000A_0;
+    end
 
-always#(1) clk = ~clk;
-always#(2000) I = I + 1;
+    always#(1) clk = ~clk;
+    always#(2000) I = I + 1;
 
 endmodule
